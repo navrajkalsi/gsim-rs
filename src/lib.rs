b1b82e3877a76d1de0e729c6fb6f72384fcc8a1e
Navraj Kalsi <navrajkalsi@icloud.com>
2026-02-21 08:15:22 -0500

    added codes uptill g21

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! # Parser
 //!
 //! This library combines `lexer` and `parser` functionality to parse **G-Code**.
-//! Aims to parse every **Mill** code on **Haas'** website.
+//! Aims to parse every **Mill** code listed on **[Haas'](https://example.com)** website.
+
+// TODO Split into lexer & parser explicitly
 
 #![allow(unused)]
 
@@ -38,6 +40,56 @@ pub enum GBlock {
     /// G09 Command
     /// Exact Stop for improving accuracy by checking for completion.
     ExactStop(),
+
+    /// G10 Command
+    /// Set offsets from within the program.
+    SetOffset {
+        /// Offset category
+        l: i32,
+        /// Specific offset
+        p: i32,
+        // Offset value
+        r: f64,
+        zero: Point,
+    },
+
+    /// G12/G13 Command
+    /// Mill circular shapes.
+    CirclePocket {
+        clockwise: bool,
+        /// Tool diameter
+        d: i32,
+        /// Radius of first circle
+        i: f64,
+        /// Radius of finished circle
+        k: f64,
+        /// Repeat count
+        l: i32,
+        /// Radius depth
+        q: f64,
+        /// Axial depth
+        z: f64,
+    },
+
+    /// G17 Command
+    /// Select plane parallel to both X and Y axes (**default for mills**).
+    XYPlane(),
+
+    /// G18 Command
+    /// Select plane parallel to both X and Z axes.
+    XZPlane(),
+
+    /// G19 Command
+    /// Select plane parallel to both Y and Z axes.
+    YZPlane(),
+
+    /// G20 Command
+    /// Use **imperial** units.
+    ImperialMode(),
+
+    /// G21 Command
+    /// Use **metric** units
+    MetricMode(),
 }
 
 /// Circular Interpolation helper
