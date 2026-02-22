3fc90a1ccca4e78a2be662d369e65d5ec031ad44
Navraj Kalsi <navrajkalsi@icloud.com>
2026-02-22 18:47:13 -0500

    added machine builder and trait implementations where required

--- src/lib.rs
+++ src/lib.rs
@@ -6,28 +6,77 @@
 
 #![allow(unused)]
 
-/// Represents a **3D coordinate**.
+use std::{cmp::Ordering, fmt::Display, process::exit};
+
+const MAX_TRAVELS: Point = Point {
+    x: 40.0,
+    y: 20.0,
+    z: 20.0,
+};
+
+const MIN_TRAVELS: Point = Point {
+    x: 5.0,
+    y: 5.0,
+    z: 5.0,
+};
+
+/// Represents a **3D coordinate** where every axis is required.
+#[derive(PartialEq, Default, Debug)]
 pub struct Point {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+/// Represents a **3D coordinate** where an axis can be **None**.
+#[derive(Debug)]
+pub struct PartialPoint {
     x: Option<f64>,
     y: Option<f64>,
     z: Option<f64>,
 }
 
+impl Point {
+    pub fn new(x: f64, y: f64, z: f64) -> Point {
+        Point { x, y, z }
+    }
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        println!("self: {self}, other: {other}");
+        if self.x < other.x || self.y < other.y || self.z < other.z {
+            Some(Ordering::Less)
+        } else if self.x > other.x || self.y > other.y || self.z > other.z {
+            Some(Ordering::Greater)
+        } else {
+            Some(Ordering::Equal)
+        }
+    }
+}
+
 /// Represents a **G-Code block**.
+#[derive(Debug)]
 pub enum GBlock {
     Empty,
 
     /// The block only contains coordinates.
-    Point(Point),
+    Point(PartialPoint),
 
     /// G00
     /// Linear Interpolate to new coordinates using rapid rate.
-    RapidMove(Point),
+    RapidMove(PartialPoint),
 
     /// G01
     /// Linear Interpolate to new coordinates using provided feed rate.
     FeedMove {
-        point: Point,
+        point: PartialPoint,
         f: Option<f64>,
     },
 
@@ -35,7 +84,7 @@ pub enum GBlock {
     /// Circular Interpolate to new coordinates using provided feed rate.
     ArcMove {
         clockwise: bool,
-        point: Point,
+        point: PartialPoint,
         method: CircleMethod,
         f: Option<f64>,
     },
@@ -58,11 +107,11 @@ pub enum GBlock {
 
     /// G28
     /// Return to Machine Zero point.
-    ReturnMachineZero(Point),
+    ReturnMachineZero(PartialPoint),
 
     /// G29
     /// Return from reference point.
-    ReturnReference(Point),
+    ReturnReference(PartialPoint),
 
     /// G40
     /// Cancel cutter compensation (G41/G42).
@@ -88,11 +137,11 @@ pub enum GBlock {
 
     /// G52
     /// Work coordinate system shift.
-    WorkCoordShift(Point),
+    WorkCoordShift(PartialPoint),
 
     /// G53
     /// Machine coordinate system.
-    MachineCoord(Point),
+    MachineCoord(PartialPoint),
 
     /// G54-G59
     /// Work coordinate system select.
@@ -164,32 +213,49 @@ pub enum MBlock {
 
 /// Circular Interpolation helper.
 /// Both relative point and radius must not appear in the same block.
+#[derive(Debug)]
 pub enum CircleMethod {
     /// Relative coordinate of circle center with **I, J & K**.
-    RelativePoint(Point),
+    RelativePoint(PartialPoint),
     /// Explicit radius specified with **R**.
     FixedRadius(f64),
 }
 
 /// Represents a side.
+#[derive(Debug)]
 pub enum Side {
     Left,
     Right,
 }
 
 /// Represents possible algerbric signs.
+#[derive(Debug)]
 pub enum Sign {
     Positive,
     Negative,
 }
 
 /// Represents a plane that is parallel to both the specified axes.
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub enum Plane {
     #[default]
     XY,
     YZ,
     XZ,
+}
+
+impl Display for Plane {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Plane::XY => "XY",
+                Plane::YZ => "YZ",
+                Plane::XZ => "XZ",
+            }
+        )
+    }
 }
 
 /// Represents a **Machine**.
@@ -210,34 +276,110 @@ pub struct Machine {
 }
 
 impl Machine {
-    /// Generate a new default machine instance.
-    pub fn new() -> Self {
-        Machine {
-            max_travels: Point {
-                x: Some(0.0),
-                y: Some(0.0),
-                z: Some(0.0),
-            },
-            pos: Point {
-                x: Some(0.0),
-                y: Some(0.0),
-                z: Some(0.0),
-            },
-            work_coord: Point {
-                x: Some(0.0),
-                y: Some(0.0),
-                z: Some(0.0),
-            },
-            active_gcodes: Vec::new(),
-            plane: Plane::default(),
-            rapid: true,
-            feed_rate: None,
-            absolute: true,
-            imperial: true,
-            tool: None,
-            coolant: false,
-            spindle_on: false,
-            spindle_speed: None,
+    /// Generates a new default machine instance and accepts the max dimensions of the machine.
+    ///
+    /// # Defaults
+    /// The defaults are as follows:
+    /// - **Home Position**: X0. Y0. Z0.
+    /// - **Start Position**: Home Position
+    /// - **Movement Type**: Rapid
+    /// - **Feed Rate**: Null
+    /// - **Positioning**: Absolute
+    /// - **Unit System**: Imperial
+    /// - **Current Tool**: Null
+    /// - **Coolant**: Off
+    /// - **Spindle**: Off
+    /// - **Spindle Speed**: Null
+    ///
+    /// # Errors
+    /// If the travels provided are above the **MAX_TRAVLELS** constant an error is returned
+    pub fn build(max_travels: Point) -> Result<Self, &'static str> {
+        if max_travels > MAX_TRAVELS {
+            Err("Machine travels provided are too large.")
+        } else if max_travels < MIN_TRAVELS {
+            Err("Machine travels have to a resonable positive number.")
+        } else {
+            Ok(Machine {
+                max_travels,
+                pos: Point::default(),
+                work_coord: Point::default(),
+                active_gcodes: Vec::new(),
+                plane: Plane::default(),
+                rapid: true,
+                feed_rate: None,
+                absolute: true,
+                imperial: true,
+                tool: None,
+                coolant: false,
+                spindle_on: false,
+                spindle_speed: None,
+            })
         }
     }
+}
+
+impl Display for Machine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Machine specs & state:\nStroke Limits: {}\nCurrent Position: {}\nCurrent Plane: {}",
+            self.max_travels, self.pos, self.plane
+        )
+    }
+}
+
+pub enum Alarm {
+    OvertravelXNeg(&'static str),
+    OvertravleXPos(&'static str),
+    OvertravelYNeg(&'static str),
+    OvertravelYPos(&'static str),
+    OvertravelZNeg(&'static str),
+    OvertravelZPos(&'static str),
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    EndBlockDetected,
+    IllegalChar,
+    NonUsableChar,
+}
+
+/// Parse a single G-Code line.
+/// Assumes that the lines have been split around ';' and the function is receiving one of those
+/// lines.
+///
+/// # Errors
+/// - EndBlockDetected: ';' should not be present.
+/// - IllegalChar: Non-Ascii char is detected.
+/// - NonUsableChar: An ascii char, that is not applicable to this usecase, is detected.
+///
+pub fn parse_block(block: &str) -> Result<GBlock, ParseError> {
+    println!("Received block: {block}");
+    if !block.is_ascii() {
+        return Err(ParseError::IllegalChar);
+    }
+
+    if block.contains(';') {
+        return Err(ParseError::EndBlockDetected);
+    }
+
+    let mut res = GBlock::Empty;
+    // this buffer stores an ascii letter and the following numbers
+    // it is reset when a space or another letter is detected
+    //
+    let mut buffer: [u8; 5] = [0, 0, 0, 0, 0];
+
+    for char in block.as_bytes() {
+        if char.is_ascii_control() {
+            return Err(ParseError::NonUsableChar);
+        }
+
+        let mut h = vec![];
+        h.push(0);
+
+        println!("{char}");
+    }
+
+    exit(0);
+    Ok(res)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
+use gsim_rs::{Machine, Point, parse_block};
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("gcode.nc").unwrap();
+    println!("{contents}");
+    for line in contents.split(';').map(|x| x.trim()) {
+        println!("{:?}", parse_block(line).unwrap());
+    }
+
+    let travels = Point::new(10.0, 10.0, 30.0);
+    let machine = Machine::build(travels).expect("Machine build error");
+
+    println!("Machine: {machine}");
 }
