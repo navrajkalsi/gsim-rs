aacd2750e87732d8231c7708db80766b704b00ec
Navraj Kalsi <navrajkalsi@icloud.com>
2026-03-04 08:13:54 -0500

    parsing codes not consumed by the parser after parsing G and M codes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 // #![warn(missing_docs)]
 use std::fs;
 
-use gsim_rs::{Machine, Point, parse_block};
+use crate::parser::{lexer::tokenize, parser::parse};
 
 mod parser;
 
 fn main() {
-    let contents = fs::read_to_string("gcode.nc").unwrap();
-    println!("{contents}");
-    for line in contents.split(';').map(|x| x.trim()) {
-        println!("{:?}", parse_block(line).unwrap());
+    let contents = fs::read_to_string("program.nc").unwrap();
+
+    for line in contents.lines() {
+        let line = line.trim().trim_end_matches(';');
+
+        println!("\nParsing: {line}");
+
+        match tokenize(line) {
+            Ok(vector) => {
+                println!("Tokens Ok: {vector:?}");
+
+                match parse(vector) {
+                    Ok(codes) => println!("Codes Ok: {codes:?}"),
+                    Err(e) => println!("Codes Error: {e:?}"),
+                }
+            }
+            Err(e) => println!("Tokens Error: {e:?}"),
+        }
     }
-
-    let travels = Point::new(10.0, 10.0, 30.0);
-    let machine = Machine::build(travels).expect("Machine build error");
-
-    println!("Machine: {machine}");
 }
